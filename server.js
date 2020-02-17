2855d1c1aeb15489739c1d5a81a64534544bbc38
sun-shadow <sun-shadow@gmail.com>
2020-02-17 22:46:21 +0800

    添加README.md

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ let url = require('url')
 let fs = require('fs')
 
 let msgQueue = []
+console.log('浏览器打开 http://localhost:3001')
 http.createServer((req, res)=>{
-  var pathObj = url.parse(req.url, true)
-	if(pathObj.pathname === '/') {
-	  fs.readFile(__dirname + '/index.html', function(err, data){
-      res.writeHead(200)
-      res.end(data)
-	  })
-	} else if(pathObj.pathname === '/send'){
+  let pathObj = url.parse(req.url, true)
+	if(pathObj.pathname === '/send'){
 		console.log(pathObj.query.msg)
     msgQueue.push(pathObj.query.msg)
     res.end('ok')
   } else if(pathObj.pathname === '/getMsg') {
   	res.end(JSON.stringify(msgQueue))
-  }
+  } else {
+	  fs.readFile(__dirname + '/index.html', function(err, data){
+      res.writeHead(200)
+      res.end(data)
+	  })
+	}
 }).listen(3001)
 
